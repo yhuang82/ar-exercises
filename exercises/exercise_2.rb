fe1a1e597ccce3876b93cc5bf507726b07b0d18f
Yiqun Huang <albert913388672@gmail.com>
2023-11-13 17:40:28 -0800

    exercise2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# Output the original names
+puts "Original names: #{@store1.name}, #{@store2.name}"
+
+@store1.update(name: "wsl")
+
+puts "Updated names: #{@store1.reload.name}, #{@store2.name}"
