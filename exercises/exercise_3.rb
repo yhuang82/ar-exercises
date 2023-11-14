066be986db7d206e4bbe6aad7e537357a3e9e5c0
Yiqun Huang <albert913388672@gmail.com>
2023-11-13 17:44:22 -0800

    exercise3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+# Output the count before deletion
+puts "Number of stores before deletion: #{Store.count}"
+
+@store3.destroy
+
+# Output the count before deletion
+puts "Number of stores before deletion: #{Store.count}"
